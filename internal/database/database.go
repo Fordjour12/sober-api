81d33a17ae85b8a064ede349b16af9497ab1dff2
Fordjour12 <fordjourbobiem@gmail.com>
2024-04-26 13:45:33 +0000

    Table creation
    Creating of database table using sql queries

--- internal/database/database.go
+++ internal/database/database.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sober-api/internal/helper"
 	"time"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
@@ -14,8 +15,8 @@ import (
 
 type Service interface {
 	Health() map[string]string
-	// CreateOnBoardingFlow(*helper.OnBoardingRequest) error
-	// CreateAccountFlow(ac *helper.CreateAccountRequest) error
+	CreateOnBoardingFlow(*helper.OnBoardingRequest) error
+	CreateAccountFlow(ac *helper.CreateAccountRequest) error
 }
 
 type service struct {
@@ -28,10 +29,14 @@ var (
 	username = os.Getenv("DB_USERNAME")
 	port     = os.Getenv("DB_PORT")
 	host     = os.Getenv("DB_HOST")
+
+	database_uri = os.Getenv("DATABASE_URL")
 )
 
 func New() Service {
-	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", username, password, host, port, database)
+	// connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", username, password, host, port, database)
+	connStr := fmt.Sprint(database_uri)
+
 	db, err := sql.Open("pgx", connStr)
 	if err != nil {
 		log.Fatal(err)
@@ -40,7 +45,48 @@ func New() Service {
 		db: db,
 	}
 
+	if err := s.init(); err != nil {
+		log.Fatalf("Error Creating Database Tables: %+v\n", err)
+	}
+
 	return s
+}
+
+// NOTE: Creation of Database tables
+
+func (s *service) init() error {
+
+	createTableQueries := []string{
+		` create table if not exists users(
+	id serial primary key,
+	username varchar(150) not null,
+	email varchar(200) not null,
+	password varchar(200) not null,
+	created_at timestamp with time zone default current_timestamp not null
+
+		)`,
+
+		` create table if not exists onboarding(
+	id serial primary key,
+	user_id int not null, 
+	reason text not null,
+	sober_date varchar(50) not null,
+	created_at timestamp with time zone default current_timestamp not null,
+
+		constraint fk_user
+			foreign key (user_id)
+			references users(id)
+			on delete cascade
+		)`,
+	}
+
+	for _, query := range createTableQueries {
+		if _, err := s.db.Exec(query); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (s *service) Health() map[string]string {
@@ -55,4 +101,13 @@ func (s *service) Health() map[string]string {
 	return map[string]string{
 		"message": "It's healthy",
 	}
+}
+
+func (s *service) CreateOnBoardingFlow(*helper.OnBoardingRequest) error {
+
+	return nil
+}
+
+func (s *service) CreateAccountFlow(ac *helper.CreateAccountRequest) error {
+	return nil
 }
